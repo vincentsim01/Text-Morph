6bb6446d7deddbd18273fc7a72344e2e32bdb007
Vincent Kiathadi <vincentkiathadi@gmail.com>
2023-08-20 22:02:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -25,10 +25,62 @@ let cooldown = cooldownTime;
 elts.text1.textContent = texts[textIndex % texts.length];
 elts.text2.textContent = texts[(textIndex + 1) % texts.length];
 
+/*
+function doMorph() {
+	morph -= cooldown;
+	cooldown = 0;
+	
+	let fraction = morph / morphTime;
+	
+	if (fraction > 1) {
+		cooldown = cooldownTime;
+		fraction = 1;
+	}
 
-console.log("this works");
-console.log(texts.length);
-console.log(time);
+*/
+
+
+
+function setMorph() {
+
+	console.log("setMorph called")
+	fraction = 0.5;
+	textIndex += 1;
+	elts.text1.textContent = texts[textIndex % texts.length];
+	elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+
+	
+	// elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+	// elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+	elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+	elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+	
+	fraction = 1 - fraction;
+	elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+	elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+	
+
+
+}
+
+function doCooldown() {
+	
+	elts.text2.style.filter = "";
+	elts.text2.style.opacity = "100%";
+	
+	elts.text1.style.filter = "";
+	elts.text1.style.opacity = "0%";
+}
+
+const timerID=setInterval(setMorph,1000);
+const timerID2=setInterval(doCooldown,1300);
+
+clearInterval(timerID);
+clearInterval(timerID2);
+
+
+/*
 
 function animate() {
 	//requestAnimationFrame(animate);
@@ -37,17 +89,26 @@ function animate() {
 	let shouldIncrementIndex = cooldown > 0;
 	let   dt = (newTime - time) / 1000;
 	time = newTime;
-
-
-    console.log(dt);
 	
 	cooldown -= dt;
 
-    console.log(cooldown);
-	
+	if (cooldown <= 0) {
+		if (shouldIncrementIndex) {
+			textIndex++;
+		}
+		
+		doMorph();
+	} else {
+		doCooldown();
+	}
+
 }
+*/
+
 
 // Start the animation.
-animate();
+//animate();
+
+
 
 
